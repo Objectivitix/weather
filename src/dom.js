c7d67ef6a50a91f7aef66fa498f4c7b1c310e504
Objectivitix <79152594+Objectivitix@users.noreply.github.com>
2023-06-25 14:33:22 -0400

    Render data to DOM

--- src/dom.js
+++ src/dom.js
@@ -1,8 +1,56 @@
+import { addDays, format } from "date-fns";
+
 const time = document.querySelector("[data-time]");
 const desc = document.querySelector(".basic__desc");
-const temp = document.querySelector(".basic__temp");
 const appar = document.querySelector("[data-appar]");
+const temp = document.querySelector(".basic__temp");
 const humidity = document.querySelector("[data-humidity]");
 const precip = document.querySelector("[data-precip]");
 const wind = document.querySelector("[data-wind]");
 const uv = document.querySelector("[data-uv]");
+
+const days = document.querySelectorAll(".forecast__day");
+const highs = document.querySelectorAll(".forecast__high");
+const lows = document.querySelectorAll(".forecast__low");
+
+export function renderWeek() {
+  const today = new Date();
+
+  const week = Array.from({ length: 7 }, (_, i) =>
+    format(addDays(today, i + 1), "E"),
+  );
+
+  week.forEach((day, index) => {
+    days[index].textContent = day;
+  });
+}
+
+export function render({
+  lastUpdateTime,
+  currDesc,
+  currTemp,
+  currApparTemp,
+  currHumidity,
+  currPrecip,
+  currWind,
+  currUV,
+  nextTempsMax,
+  nextTempsMin,
+}) {
+  time.textContent = lastUpdateTime;
+  desc.textContent = currDesc;
+  temp.textContent = currTemp;
+  appar.textContent = currApparTemp;
+  humidity.textContent = currHumidity;
+  precip.textContent = currPrecip;
+  wind.textContent = currWind;
+  uv.textContent = currUV;
+
+  nextTempsMax.forEach((temp, index) => {
+    highs[index].textContent = temp;
+  })
+
+  nextTempsMin.forEach((temp, index) => {
+    lows[index].textContent = temp;
+  })
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import getData from "./data";
+import { render, renderWeek } from "./dom";
 import { IMPERIAL, METRIC, process } from "./process";
+
+renderWeek();
 
 getData("Kanata")
   .then((data) => process(data, METRIC))
-  .then(console.log);
+  .then(render);
 
 getData("Kanata", true)
   .then((data) => process(data, IMPERIAL))
